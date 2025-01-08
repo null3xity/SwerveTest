c5009ccd29e8e646932c4357222fd3c1de2ccb51
your name <kellonisabeast1@gmail.com>
2025-01-07 18:48:50 -0600

    first commit

---
+++ src/main/java/frc/robot/Gyroscope.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class Gyroscope {
+
+    public Rotation2d getAngle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAngle'");
+    }
+
+}
---
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -0,0 +1,85 @@
+package frc.robot;
+
+// Import relevant classes.
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class SwerveDrive extends SubsystemBase
+{
+
+    SwerveDriveKinematics kinematics;
+    SwerveDriveOdometry   odometry;
+    Gyroscope             gyro; 
+    SwerveModule[]        swerveModules;
+    
+    // Constructor
+    public SwerveDrive() 
+    {
+    
+        
+        kinematics = new SwerveDriveKinematics(
+            new Translation2d(Units.inchesToMeters(12.5), Units.inchesToMeters(12.5)), // Front Left
+            new Translation2d(Units.inchesToMeters(12.5), Units.inchesToMeters(-12.5)), // Front Right
+            new Translation2d(Units.inchesToMeters(-12.5), Units.inchesToMeters(12.5)), // Back Left
+            new Translation2d(Units.inchesToMeters(-12.5), Units.inchesToMeters(-12.5))  // Back Right
+        );
+        
+        gyro = new Gyroscope(); 
+
+        
+        odometry = new SwerveDriveOdometry(
+            kinematics,
+            gyro.getAngle(), 
+            new SwerveModulePosition[]{new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition()},
+            
+            new Pose2d(0,0,new Rotation2d()) 
+        );
+            
+    }
+    
+    // Simple drive function
+    public void drive()
+    { 
+        ChassisSpeeds testSpeeds = new ChassisSpeeds(Units.inchesToMeters(14), Units.inchesToMeters(4), Units.degreesToRadians(30));
+        
+        // Get the SwerveModuleStates for each module given the desired speeds.
+        SwerveModuleState[] swerveModuleStates = kinematics.toSwerveModuleStates(testSpeeds);
+        // Output order is Front-Left, Front-Right, Back-Left, Back-Right
+        
+        swerveModules[0].setDesiredState(swerveModuleStates[0]);
+        swerveModules[1].setDesiredState(swerveModuleStates[1]);
+        swerveModules[2].setDesiredState(swerveModuleStates[2]);
+        swerveModules[3].setDesiredState(swerveModuleStates[3]);
+    }
+    
+    // Fetch the current swerve module positions.
+    public SwerveModulePosition[] getCurrentSwerveModulePositions()
+    {
+        return new SwerveModulePosition[] {
+            new SwerveModulePosition(swerveModules[0].getDistance(), swerveModules[0].getAngle()), // Front-Left
+            new SwerveModulePosition(swerveModules[1].getDistance(), swerveModules[1].getAngle()), // Front-Right
+            new SwerveModulePosition(swerveModules[2].getDistance(), swerveModules[2].getAngle()), // Back-Left
+            new SwerveModulePosition(swerveModules[3].getDistance(), swerveModules[3].getAngle())  // Back-Right
+        };
+    }
+    
+    @Override
+    public void periodic()
+    {
+        // Update the odometry every run.
+        
+
+
+    }
+    
+}
